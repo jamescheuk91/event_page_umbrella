520e2145320de59c30d5d2761b3c6f7c24d41149
James Cheuk <jccf091@gmail.com>
2017-07-01 18:25:15 +0800

    extract event detail form fields

--- apps/event_page_web/lib/event_page_web/views/event_view.ex
+++ apps/event_page_web/lib/event_page_web/views/event_view.ex
@@ -1,9 +1,13 @@
 defmodule EventPage.Web.EventView do
   use EventPage.Web, :view
-  
+
   alias EventPage.Web.EventDetailView
 
   def render("event_detail_show.html", %{event_detail: event_detail}) do
     render(EventDetailView, "show.html", %{event_detail: event_detail})
+  end
+
+  def render("event_detail_form_fields", %{f: f}) do
+    render(EventDetailView, "form_fields.html", %{f: f})
   end
 end
